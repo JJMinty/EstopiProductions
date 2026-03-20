d5b3ffb8d6c86447394fa80b8072cd0e8b745cad
JJMinty <97975203+JJMinty@users.noreply.github.com>
2026-03-19 22:32:39 -0400

    Create main.jsx file that imports React and ReactDOM, rendering AIAgentSalesWebsite.

---
+++ main.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AIAgentSalesWebsite from './AIAgentSalesWebsite';
+
+ReactDOM.render(<AIAgentSalesWebsite />, document.getElementById('root'));
